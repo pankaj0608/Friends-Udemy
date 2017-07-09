6b92b7a97af9be45d3ac2b75d06a39a2966d0237
rsoni123 <Amaya123>
2017-07-09 19:30:39 -0400

    Code changed

--- app/src/main/java/com/example/pankaj/friends/FriendsContract.java
+++ app/src/main/java/com/example/pankaj/friends/FriendsContract.java
@@ -1,7 +1,47 @@
 package com.example.pankaj.friends;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
 /**
  * Created by pankaj on 7/9/2017.
  */
 public class FriendsContract {
+
+    interface FriendsColumns {
+        String FRIENDS_NAME = "friends_name";
+        String FRIENDS_EMAIL = "friends_email";
+        String FRIENDS_PHONE = "friends_phone";
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.example.pankaj.friends.provider";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String PATH_FRIENDS = "friends";
+
+    private static final String[] TOP_LEVEL_PATHS = {
+            PATH_FRIENDS
+    };
+
+
+    public static class Friends implements FriendsColumns, BaseColumns {
+
+        public static final Uri CONTENT_URI
+                = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_FRIENDS).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + ".friends";
+
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + ".friends";
+
+        public static Uri buildFriendUri (String friendId) {
+            return CONTENT_URI.buildUpon().appendEncodedPath(friendId).build();
+        }
+
+        public static String getFriendId(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+    }
 }
