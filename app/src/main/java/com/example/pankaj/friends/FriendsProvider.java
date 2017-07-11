205518528462c6a40eb768564aeba7efbfe907dd
rsoni123 <Amaya123>
2017-07-11 14:14:44 -0400

    Code changed

--- app/src/main/java/com/example/pankaj/friends/FriendsProvider.java
+++ app/src/main/java/com/example/pankaj/friends/FriendsProvider.java
@@ -4,7 +4,10 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by pankaj on 7/9/2017.
@@ -28,21 +31,55 @@ public class FriendsProvider extends ContentProvider {
 
         return matcher;
     }
+
     @Override
     public boolean onCreate() {
         friendsDatabase = new FriendsDatabase(getContext());
         return true;
     }
 
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        return null;
-    }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = uriMatcher.match(uri);
+        switch (match) {
+            case FRIENDS:
+                return FriendsContract.Friends.CONTENT_TYPE;
+
+            case FRIENDS_ID:
+                return FriendsContract.Friends.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new IllegalArgumentException("Unknown Uri " + uri);
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = friendsDatabase.getReadableDatabase();
+        final int match = uriMatcher.match(uri);
+
+        SQLiteQueryBuilder sqLiteQueryBuilder = new SQLiteQueryBuilder();
+        sqLiteQueryBuilder.setTables(FriendsDatabase.Tables.FRIENDS);
+
+        switch (match) {
+            case FRIENDS:
+                break;
+
+            case FRIENDS_ID:
+                String id=FriendsContract.Friends.getFriendId(uri);
+                sqLiteQueryBuilder.appendWhere(BaseColumns._ID + "=" + id);
+                break;
+
+            default:
+                break;
+        }
+
+        return sqLiteQueryBuilder.query(
+                friendsDatabase.getReadableDatabase(), projection,
+                selection,selectionArgs,
+                null,null,sortOrder);
     }
 
     @Override
@@ -62,7 +99,8 @@ public class FriendsProvider extends ContentProvider {
     }
 
     private void deleteDatabase() {
-        friendsDatabase.close();;
+        friendsDatabase.close();
+        ;
         FriendsDatabase.deleteDatabase(getContext());
         friendsDatabase = new FriendsDatabase(getContext());
     }
